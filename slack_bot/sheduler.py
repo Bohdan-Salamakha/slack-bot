eacba9f3b6e89254f2a4765ff8f460c891e0bc83
Bohdan Salamakha <allen.avanheim@gmail.com>
2023-02-01 23:02:41 +0200

    fixed schedule bug

--- slack_bot/sheduler.py
+++ slack_bot/sheduler.py
@@ -1,7 +1,7 @@
 import threading
 from time import sleep
 
-from schedule import run_pending, every
+from schedule import run_pending, every, ScheduleValueError
 
 from slack_bot.bot import SlackBot
 from slack_bot.settings import WORK_TIME_START, WORK_TIME_FINISH, TIME_CHECK_INTERVAL
@@ -16,8 +16,16 @@ class Scheduler:
     @staticmethod
     def run():
         bot = SlackBot()
-        start_work = (every().minute.until(WORK_TIME_FINISH).
-                      do(Scheduler.run_threaded, bot.check_schedule))
+        while True:
+            try:
+                start_work = (every().minute.until(WORK_TIME_FINISH).
+                              do(Scheduler.run_threaded, bot.check_schedule))
+            except ScheduleValueError:
+                print(f'Can not set WORK_TIME_FINISH={WORK_TIME_FINISH}. '
+                      f'Wait for tomorrow please.')
+                sleep(TIME_CHECK_INTERVAL * 60)
+            else:
+                break
         every().day.at(WORK_TIME_START).do(Scheduler.run_threaded, start_work)
         while True:
             run_pending()
